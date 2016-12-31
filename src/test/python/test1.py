fa57a1cc33aef56b615cd9ba6e64bf0022a9549a
paul.balm <paul.balm@amadeus.com>
2016-12-31 19:11:38 +0100

    Fixing indents

--- src/test/python/test1.py
+++ src/test/python/test1.py
@@ -5,14 +5,14 @@ from datetime import datetime
 import pandas.io.data as web
 
 df = web.DataReader("aapl", 'yahoo',
-    datetime(2007, 10, 1),
-    datetime(2009, 4, 1))
+                    datetime(2007, 10, 1),
+                    datetime(2009, 4, 1))
 
 trace = go.Scatter(x=[1, 2, 3, 4, 5, 6, 7, 8, 9],
                    y=[2, 3, 4, 5, 9, 0, 9, 9, 1],
-    error_y=dict(type='data',
-        array=[1, 2, 1, 2, 1, 2, 1, 2, 3],
-        visible=True))
+                   error_y=dict(type='data',
+                                array=[1, 2, 1, 2, 1, 2, 1, 2, 3],
+                                visible=True))
 
 data = [trace]
 layout = dict(
